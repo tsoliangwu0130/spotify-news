19d3bd4b35492b2ff65134ea985eecdbc250327f
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-08-09 11:03:57 -0700

    Authorization - get token

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, request
 from urllib.parse import quote_plus, urlencode
 
+import requests
+
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -12,26 +14,36 @@ PORT = 8888
 REDIRECT_URI = '{}:{}/callback'.format(CLIENT_SIDE_URL, PORT)
 
 SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
+SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
 
 
 @app.route('/')
 def index():
-    payload = {
+    auth_payload = {
         'client_id': CLIENT_ID,
         'response_type': 'code',
         'redirect_uri': REDIRECT_URI
     }
     encoded_auth_url = '{}/?{}'.format(
         SPOTIFY_AUTH_URL,
-        urlencode(payload, quote_via=quote_plus)
+        urlencode(auth_payload, quote_via=quote_plus)
     )
     return redirect(encoded_auth_url)
 
 
 @app.route('/callback')
 def callback():
-    auth_code = request.args['code']
-    print('>>>>> auth_code: ', auth_code)
+    token_payload = {
+        'grant_type': 'authorization_code',
+        'code': request.args['code'],
+        'redirect_uri': REDIRECT_URI
+    }
+    result = requests.post(
+        SPOTIFY_TOKEN_URL,
+        data=token_payload,
+        auth=(CLIENT_ID, CLIENT_SECRET)
+    )
+    print('>>>', result.text)
     return 'Hello callback'
 
 
