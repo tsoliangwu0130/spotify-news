49e72747e27174ac7146c9408aa42a69d0d2797e
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-08-09 10:19:14 -0700

    Authorization - callback

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-from flask import Flask
+from flask import Flask, redirect, request
+from urllib.parse import quote_plus, urlencode
 
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+
+CLIENT_SIDE_URL = 'http://localhost'
+PORT = 8888
+REDIRECT_URI = '{}:{}/callback'.format(CLIENT_SIDE_URL, PORT)
+
+SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    payload = {
+        'client_id': CLIENT_ID,
+        'response_type': 'code',
+        'redirect_uri': REDIRECT_URI
+    }
+    encoded_auth_url = '{}/?{}'.format(
+        SPOTIFY_AUTH_URL,
+        urlencode(payload, quote_via=quote_plus)
+    )
+    return redirect(encoded_auth_url)
+
+
+@app.route('/callback')
+def callback():
+    auth_code = request.args['code']
+    print('>>>>> auth_code: ', auth_code)
+    return 'Hello callback'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=PORT)
